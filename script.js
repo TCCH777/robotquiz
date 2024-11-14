4f13a8d66c79531a7061a90092a108a1f0cf92f8
italo <italocianci00@gmail.como>
2024-11-14 15:32:56 -0500

    muestra imagen, falta que funcione

--- script.js
+++ script.js
@@ -13,7 +13,49 @@ function nextQuestion() {
     // Muestra la nueva pregunta
     questions[currentQuestionIndex].classList.add("active");
   }
+  if (currentQuestionIndex == 3) {
+    showResult();
+  }
 }
 
 // Inicializa mostrando solo la primera pregunta
 questions[currentQuestionIndex].classList.add("active");
+
+let answers = {
+  size: "",
+  purpose: "",
+};
+
+// Función para mostrar el resultado basado en las respuestas
+function showResult() {
+  let robotImage = "";
+
+  // Condiciones para seleccionar la imagen correcta del robot
+  if (answers.size === "pequeño" && answers.purpose === "decoracion") {
+    robotImage = "./images/Robot Arte.png";
+  } else if (answers.size === "grande" && answers.purpose === "decoracion") {
+    robotImage = "./images/Robot tiempo.png";
+  } else if (answers.size === "grande" && answers.purpose === "amigo") {
+    robotImage = "./images/Robot Auroi.png";
+  } else if (answers.size === "pequeño" && answers.purpose === "amigo") {
+    robotImage = "./images/Robot Coram.png";
+  } else if (answers.size === "grande" && answers.purpose === "asistente") {
+    robotImage = "./images/Robot elisa.png";
+  } else if (
+    answers.size === "pequeño" &&
+    answers.purpose === "arma de guerra"
+  ) {
+    robotImage = "./images/Robot milagro.png";
+  } else if (
+    answers.size === "grande" &&
+    answers.purpose === "arma de guerra"
+  ) {
+    robotImage = "./images/Robot samurai.png";
+  } else if (answers.size === "pequeño" && answers.purpose === "asistente") {
+    robotImage = "./images/Robot sumaqui.png";
+  }
+
+  // Mostrar el contenedor de respuesta y establecer la imagen del robot seleccionado
+  document.getElementById("answer-container").style.display = "block";
+  document.getElementById("answer-img").src = robotImage;
+}
